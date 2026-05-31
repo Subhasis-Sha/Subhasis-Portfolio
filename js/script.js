1181a25c750ed03dbdf9c674909692ac7326b950
Subhasis-Sha <subhasissha991@gmail.com>
2026-06-01 02:59:43 +0530

    Added all files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: "#vantajs",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x00ffff,
+    backgroundColor: 0x050816,
+    points: 12.00,
+    maxDistance: 22.00,
+    spacing: 18.00
+})
